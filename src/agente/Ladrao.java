103c3d71e79d64d169c5903dcb09a604960cc9a2
Pedroklemensov <pedroklemensov@gmail.com>
2023-10-18 16:20:56 -0300

    a

--- src/agente/Ladrao.java
+++ src/agente/Ladrao.java
@@ -3,9 +3,25 @@ package agente;
 import algoritmo.ProgramaLadrao;
 
 public class Ladrao extends ProgramaLadrao {
+
 	
+
 	public int acao() {
-		
+
+		System.out.println("inicio");
+		for (int i = 0; i < sensor.getVisaoIdentificacao().length; i++) {
+			System.out.print(sensor.getVisaoIdentificacao()[i]);
+
+			if ((i + 1) % 5 == 0) {
+				System.out.println();
+			}
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println("fim");
+		System.out.println(sensor.getPosicao());
+
 		return (int) (Math.random() * 5);
 	}
 
